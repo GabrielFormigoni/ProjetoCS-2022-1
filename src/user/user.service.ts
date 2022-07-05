0866ef6895a2408c6a90b1c6d38498b634ce3c43
Gabriel Formigoni <bielformigoni@gmail.com>
2022-07-05 17:16:05 -0300

    Projeto

---
+++ src/user/user.service.ts
@@ -0,0 +1,76 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/database/PrismaService';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+
+  constructor(private prisma: PrismaService){
+
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prisma.user.create({
+      data: createUserDto,
+    });
+
+    return user;
+  }
+
+  async findAll() {
+    return await this.prisma.user.findMany({
+      orderBy: {
+        id: 'asc'
+      }
+    });
+  }
+
+  async findOne(id: number, req) {
+
+    if(req.user.id != id){
+      throw new UnauthorizedException();
+    }
+
+    return await this.prisma.user.findFirst({
+      where:{
+        id
+      }
+    });
+  }
+
+  async findOneByEmail(email: string) {
+    return await this.prisma.user.findUnique({
+      where:{
+        email: email
+      }
+    });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto, req) {
+
+    if(req.user.id != id){
+      throw new UnauthorizedException();
+    }
+    return this.prisma.user.update({
+      where: {
+        id
+      },
+      data: updateUserDto
+    });
+  }
+
+  remove(id: number, req) {
+
+    if(req.user.id != id){
+      throw new UnauthorizedException();
+    }
+
+    return this.prisma.user.delete({
+      where: {
+        id
+      }
+    });
+  }
+}
